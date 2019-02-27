3cd46f090e3be91c7b0c45ad94a90bffacd031e4
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2019-02-27 03:18:49 +0200

    code comments

--- eco-lab/2.py
+++ eco-lab/2.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python -W ignore
 import numpy as np
 
+# матриця Леслі
 L = np.array([
 	[  0,   6, 15],
 	[0.5,   0,  0],
 	[  0, 0.5,  0]
 ])
 
+# початковий розподіл популяції
 x0 = np.array([1, 1, 1])
 
-print(f'x(5) = {np.dot(np.linalg.matrix_power(L, 5), x0)}')
+# розподіл через t = 5 кроків обчислюємо як L^t * x0
+t = 5
+print(f'x({t}) = {np.dot(np.linalg.matrix_power(L, t), x0)}')
 
-λ, x = np.linalg.eig(L)
+# знаходимо власні значення і власні вектори
+_lambda, x = np.linalg.eig(L)
+# знаходимо найбільше додатне власне значення
+lambda_L = np.max(_lambda)
 
-λ_L = np.max(λ)
+print(f'lambda_L = {np.float_(lambda_L)}')
 
-print(f'λ_L = {np.float_(λ_L)}')
-
-x_L = x[:, λ == λ_L].T[0] 
-
+# знаходимо власний вектор що йому відповідає
+x_L = x[:, _lambda == lambda_L].T[0] 
+# нормуємо цей вектор в || ||_1 нормі
 x_L /= np.linalg.norm(x_L, 1)
 
 print(f'x_L = {np.abs(np.float_(x_L))}')
 
+# знаходимо min t: ||x(t)||_1 > 75
 t = 0
 while np.sum(x0) <= 75:
 	x0 = np.dot(L, x0)
